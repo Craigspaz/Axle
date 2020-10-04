f5346d679a050220d162f7acb46b495f2e17d235
Craig Ferris <cmferris97@gmail.com>
2020-10-04 19:38:23 -0400

    Added functions to radio buttons

--- include/Menu-UI/RadioButton.h
+++ include/Menu-UI/RadioButton.h
@@ -2,18 +2,35 @@
 #define __RADIOBUTTON__
 #include "UIElement.h"
 #include <iostream>
+#include <vector>
 
 class RadioButton : public UIElement
 {
 public:
-    RadioButton(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale, bool visible = true, bool disabled = false);
+    RadioButton(std::vector<std::string> options, int numberOfOptions, glm::vec3 position, glm::vec3 rotation, glm::vec3 scale, bool visible = true, bool disabled = false);
     ~RadioButton();
+
+    std::vector<std::string> getOptions();
+    void addOption(std::string option);
+    void removeOption(std::string option);
+    void setOptions(std::vector<std::string> options);
+
+    std::string getSelectedValue();
+    std::string getDefaultValue();
+    void setDefaultOption(int option);
+    void setSelectedOption(int option);
+    int getSelectedOption();
+    int getDefaultOption();
 
     UIElement toParentObject() const;
     bool operator==(const RadioButton &radioButton);
     bool operator!=(const RadioButton &radioButton);
 
 private:
+    std::vector<std::string> _options; // Array of options
+    int _selected;
+    int _default;
+
 protected:
 };
 
--- src/Menu-UI/RadioButton.cpp
+++ src/Menu-UI/RadioButton.cpp
@@ -1,8 +1,68 @@
 #include "Menu-UI/RadioButton.h"
 
-RadioButton::RadioButton(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale, bool visible, bool disabled)
-    : UIElement(position, rotation, scale, visible, disabled)
+RadioButton::RadioButton(std::vector<std::string> options, int numberOfOptions, glm::vec3 position, glm::vec3 rotation, glm::vec3 scale, bool visible, bool disabled)
+    : UIElement(position, rotation, scale, visible, disabled),
+      _options(options),
+      _default(0),
+      _selected(0)
 {
+}
+
+std::vector<std::string> RadioButton::getOptions()
+{
+    return _options;
+}
+
+void RadioButton::addOption(std::string option)
+{
+    _options.push_back(option);
+}
+
+void RadioButton::removeOption(std::string option)
+{
+    for (int i = 0; i < _options.size(); i++)
+    {
+        if (_options[i].compare(option) == 0)
+        {
+            _options.erase(_options.begin() + i);
+            break;
+        }
+    }
+}
+
+void RadioButton::setOptions(std::vector<std::string> options)
+{
+    _options = options;
+}
+
+std::string RadioButton::getSelectedValue()
+{
+    return _options[_selected];
+}
+
+std::string RadioButton::getDefaultValue()
+{
+    return _options[_default];
+}
+
+void RadioButton::setDefaultOption(int option)
+{
+    _default = option;
+}
+
+void RadioButton::setSelectedOption(int option)
+{
+    _selected = option;
+}
+
+int RadioButton::getSelectedOption()
+{
+    return _selected;
+}
+
+int RadioButton::getDefaultOption()
+{
+    return _default;
 }
 
 RadioButton::~RadioButton()
@@ -16,7 +76,27 @@ UIElement RadioButton::toParentObject() const
 
 bool RadioButton::operator==(const RadioButton &radioButton)
 {
-    return (this->toParentObject() == radioButton.toParentObject());
+    bool part1 = (this->_default == radioButton._default && this->_selected == radioButton._selected && this->_options.size() == radioButton._options.size() && this->toParentObject() == radioButton.toParentObject());
+    bool elements = false;
+
+    if (part1)
+    {
+        bool brokeOut = false;
+        for (int i = 0; i < this->_options.size(); i++)
+        {
+            if (this->_options[i] != radioButton._options[i])
+            {
+                brokeOut = true;
+                break;
+            }
+        }
+        if (!brokeOut)
+        {
+            elements = true;
+        }
+    }
+
+    return part1 && elements;
 }
 
 bool RadioButton::operator!=(const RadioButton &radioButton)
